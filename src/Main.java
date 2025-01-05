79e065c37fdcec7fef0c5ba884e654bfefa8e363
Kiritto01 <patgiz204@gmail.com>
2025-01-05 15:27:34 +0100

    Initiala commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        ExceptionGenerator generator = new ExceptionGenerator();
+        try {
+            generator.generateNullPointerException();
+        } catch (NullPointerException e) {
+            System.out.println("Wystąpił wyjątek NullPointerException");
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+    }
+}
